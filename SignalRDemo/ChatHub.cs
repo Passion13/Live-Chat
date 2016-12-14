428e314cf7c6cc34b8b3337fe8a156ed519fda94
Anant Pithadiya <anantpithadiya@rangamindia.com>
2016-12-14 07:02:46 -0500

    First Commit

---
+++ SignalRDemo/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        //public void Hello()
+        //{
+        //    Clients.All.hello();
+        //}
+        public void Send(string name, string message)
+        {
+            
+            Clients.All.broadcastMessage(name, message);
+            System.Console.WriteLine(message);
+        }
+    }
+}
